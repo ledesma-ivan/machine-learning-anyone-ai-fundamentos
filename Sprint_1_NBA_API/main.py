592ef81f113ee6e413b8a68efd5215dbc1cdcdf1
ledesma-ivan <ivanarielledesma@gmail.com>
2025-06-20 09:10:53 -0300

    Limitados los datos de jugadores a 450.

--- Sprint_1_NBA_API/main.py
+++ Sprint_1_NBA_API/main.py
@@ -1,5 +1,3 @@
-# src/main.py
-
 import os
 from src.data_ingestion.fetch_data import fetch_active_players, fetch_all_players_info
 from src.utils.logger import setup_logger
@@ -7,7 +5,6 @@ from src.utils.logger import setup_logger
 def main():
     logger = setup_logger("main")
 
-    # Carpeta de salida
     output_dir = "data/raw"
     os.makedirs(output_dir, exist_ok=True)
 
@@ -15,17 +12,17 @@ def main():
     players_path = os.path.join(output_dir, "active_players.csv")
     players_df = fetch_active_players(save_path=players_path)
 
-    # Mostrar columnas disponibles para debug
     logger.info(f"Columnas disponibles: {players_df.columns.tolist()}")
 
-    # Filtrar los primeros 10 jugadores
-    top_players_df = players_df.head(10)
-    logger.info(f"Se seleccionaron los primeros 10 jugadores activos: {top_players_df['PERSON_ID'].tolist()}")
+    #Paso 1.5: Limitar a los primeros N jugadores activos
+    max_players = 450
+    limited_df = players_df.head(max_players)
+    logger.info(f"Se seleccionaron los primeros {max_players} jugadores activos: {limited_df['PERSON_ID'].tolist()}")
 
-    # Paso 2: Obtener info detallada de los jugadores (renombrar columna a 'PLAYER_ID')
-    fetch_all_players_info(top_players_df.rename(columns={"PERSON_ID": "PLAYER_ID"}), out_dir=output_dir)
+    # Paso 2: Obtener info detallada de los jugadores
+    fetch_all_players_info(limited_df.rename(columns={"PERSON_ID": "PLAYER_ID"}), out_dir=output_dir)
 
-    logger.info("Pipeline ETL completado con 10 jugadores.")
+    logger.info(f"Pipeline ETL completado con {max_players} jugadores.")
 
 if __name__ == "__main__":
     main()
